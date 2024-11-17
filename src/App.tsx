929f1c482983758411c81172a4bc2791a469b014
saebyn <saebynx+github@gmail.com>
2024-11-17 09:02:22 -0800

    initialize React project with Vite, add basic app structure and configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <p>React 18</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('No container found');
+}
+
+const root = createRoot(container);
+root.render(<App />);
