089021adda9af6653eb3d6b091cb07fc3ba15fb8
saebyn <saebynx+github@gmail.com>
2024-12-01 09:15:19 -0800

    feat: enhance TwitchOAuthButton with connect and disconnect functionality and error handling

--- src/atoms/TwitchOAuthButton.tsx
+++ src/atoms/TwitchOAuthButton.tsx
@@ -1,8 +1,9 @@
 import { getCsrfToken } from '@/csrf';
 import { generateAuthorizeUri } from '@/twitch';
 import CheckIcon from '@mui/icons-material/Check';
+import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
-import { useTranslate } from 'react-admin';
+import { LoadingIndicator, useTranslate, useUpdate } from 'react-admin';
 
 interface TwitchOAuthButtonProps {
   tokens: {
@@ -14,32 +15,72 @@ const scopes = ['channel:manage:broadcast'];
 
 function TwitchOAuthButton({ tokens }: TwitchOAuthButtonProps) {
   const translate = useTranslate();
+  const [update, { isPending, isError }] = useUpdate();
+
   const csrfToken = getCsrfToken();
   const url = generateAuthorizeUri(csrfToken, scopes);
 
-  // if the user is not connected, show the connect button
-  if (!tokens?.accessToken) {
+  const handleDisconnect = () => {
+    update('profile', {
+      id: 'my-profile',
+      data: { twitch: { accessToken: null } },
+    });
+  };
+
+  const handleConnect = () => {
+    window.location.href = url;
+  };
+
+  if (isPending) {
+    return <LoadingIndicator />;
+  }
+
+  if (isError) {
     return (
-      <Button variant="contained" color="primary" component="a" href={url}>
-        {translate('gt.profile.connectTwitch', { _: 'Connect to Twitch' })}
-      </Button>
+      <Alert severity="error">
+        {translate('gt.profile.errorSaving', {
+          _: 'There was an error saving the profile',
+        })}
+      </Alert>
     );
   }
 
   // if the user is connected, show the reauthorize button
   return (
-    <Button
-      variant="contained"
-      color="success"
-      component="a"
-      href={url}
-      title={translate('gt.profile.reauthorizeTwitch', {
-        _: 'Click to Re-authorize Twitch',
-      })}
-    >
-      <CheckIcon />{' '}
-      {translate('gt.profile.connectedToTwitch', { _: 'Connected to Twitch' })}
-    </Button>
+    <>
+      {tokens.accessToken && (
+        <>
+          <Button
+            variant="contained"
+            color="success"
+            title={translate('gt.profile.reauthorizeTwitch', {
+              _: 'Click to Re-authorize Twitch',
+            })}
+            onClick={handleConnect}
+          >
+            <CheckIcon />{' '}
+            {translate('gt.profile.connectedToTwitch', {
+              _: 'Connected to Twitch',
+            })}
+          </Button>
+          <Button
+            color="warning"
+            variant="contained"
+            onClick={handleDisconnect}
+          >
+            {translate('gt.profile.disconnectTwitch', {
+              _: 'Click to Disconnect Twitch',
+            })}
+          </Button>
+        </>
+      )}
+
+      {!tokens.accessToken && (
+        <Button color="primary" variant="contained" onClick={handleConnect}>
+          {translate('gt.profile.connectTwitch', { _: 'Connect to Twitch' })}
+        </Button>
+      )}
+    </>
   );
 }
 
