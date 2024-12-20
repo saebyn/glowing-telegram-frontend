6b235476a0d11ad2cae1ab23f2705ad0c154f686
saebyn <saebynx+github@gmail.com>
2024-12-20 08:12:59 -0800

    refactor: remove unused AsyncResultLoader, TimelineView, and StreamSilenceDetectionInput components to streamline codebase

--- src/resources/streams/Edit.tsx
+++ src/resources/streams/Edit.tsx
@@ -2,7 +2,7 @@ import {
   DateTimeInput,
   ReferenceInput,
   SelectInput,
-  TabbedForm,
+  SimpleForm,
   TextInput,
 } from 'react-admin';
 
@@ -10,70 +10,42 @@ import DescriptionInput from '@/components/atoms/DescriptionInput';
 import { TimeDurationInput } from '@/components/atoms/TimeDurationInput';
 import TitleInput from '@/components/atoms/TitleInput';
 import Edit, { type EditProps } from '@/components/templates/Edit';
-import StreamSilenceDetectionInput from './StreamSilenceDetectionInput';
-import StreamTranscriptInput from './StreamTranscriptInput';
-import StreamVideoClipsInput from './StreamVideoClipsInput';
-import TimelineView from './TimelineView';
 
 const StreamEdit = (props: EditProps) => (
   <Edit {...props}>
-    <TabbedForm>
-      <TabbedForm.Tab label="summary">
-        <TitleInput source="title" required />
+    <SimpleForm>
+      <TitleInput source="title" required />
 
-        <ReferenceInput source="series_id" reference="series">
-          <SelectInput optionText="title" />
-        </ReferenceInput>
+      <ReferenceInput source="series_id" reference="series">
+        <SelectInput optionText="title" />
+      </ReferenceInput>
 
-        <DescriptionInput source="description" />
+      <DescriptionInput source="description" />
 
-        <TextInput source="thumbnail" fullWidth parse={(value) => value} />
+      <TextInput source="thumbnail" fullWidth parse={(value) => value} />
 
-        <SelectInput
-          source="stream_platform"
-          choices={[
-            { id: 'twitch', name: 'Twitch' },
-            { id: 'youtube', name: 'YouTube' },
-          ]}
-          required
-          defaultValue="twitch"
-        />
-        <TextInput source="stream_id" />
+      <SelectInput
+        source="stream_platform"
+        choices={[
+          { id: 'twitch', name: 'Twitch' },
+          { id: 'youtube', name: 'YouTube' },
+        ]}
+        required
+        defaultValue="twitch"
+      />
+      <TextInput source="stream_id" />
 
-        <DateTimeInput source="stream_date" required />
+      <DateTimeInput source="stream_date" required />
 
-        <TimeDurationInput source="duration" />
+      <TimeDurationInput source="duration" />
 
-        <TextInput
-          source="prefix"
-          required
-          helperText="The prefix is used to identify related video clips for this stream. It's typically in the format YYYY-MM-DD."
-          inputProps={{ pattern: '[0-9]{4}-[0-9]{2}-[0-9]{2}' }}
-        />
-      </TabbedForm.Tab>
-
-      <TabbedForm.Tab label="video clips">
-        <StreamVideoClipsInput source="video_clips" />
-      </TabbedForm.Tab>
-
-      <TabbedForm.Tab label="transcript">
-        <StreamTranscriptInput
-          source="transcription_segments"
-          taskUrlFieldName="transcription_task_url"
-        />
-      </TabbedForm.Tab>
-
-      <TabbedForm.Tab label="audio">
-        <StreamSilenceDetectionInput
-          source="silence_segments"
-          taskUrlFieldName="silence_detection_task_url"
-        />
-      </TabbedForm.Tab>
-
-      <TabbedForm.Tab label="timeline">
-        <TimelineView />
-      </TabbedForm.Tab>
-    </TabbedForm>
+      <TextInput
+        source="prefix"
+        required
+        helperText="The prefix is used to identify related video clips for this stream. It's typically in the format YYYY-MM-DD."
+        inputProps={{ pattern: '[0-9]{4}-[0-9]{2}-[0-9]{2}' }}
+      />
+    </SimpleForm>
   </Edit>
 );
 
