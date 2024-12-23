df0e077bfa154b4ea735a5b93293957607b43a3c
saebyn <saebynx+github@gmail.com>
2024-12-22 16:14:48 -0800

    feat: add TypeScript interfaces for VideoClipRecord and StreamRecord in dataProvider

---
+++ src/types/dataProvider.ts
@@ -0,0 +1,69 @@
+import type { Identifier, RaRecord } from 'react-admin';
+
+export interface VideoClipRecord extends RaRecord {
+  key: string;
+  silence?: Array<{ start: number; end: number }>;
+  transcript?: {
+    language: string;
+    text: string;
+    segments: Array<{
+      start: number;
+      end: number;
+      avg_logprob: number;
+      compression_ratio: number;
+      no_speech_prob: number;
+      temperature: number;
+      text: string;
+    }>;
+  };
+  summary?: {
+    attentions: Array<{
+      timestamp_start: number;
+      timestamp_end: number;
+      description: number;
+      reasoning: number;
+    }>;
+    highlights: Array<{
+      timestamp_start: number;
+      timestamp_end: number;
+      description: number;
+      reasoning: number;
+    }>;
+    keywords: Array<string>;
+
+    summary_context: string;
+    summary_main_discussion: string;
+    title: string;
+    transcription_errors: Array<{
+      timestamp_start: number;
+      description: number;
+      reasoning: number;
+    }>;
+  };
+  start_time: number;
+  stream_id: Identifier;
+  metadata?: {
+    format: {
+      duration: number;
+      [key: string]: unknown;
+    };
+    streams: unknown;
+  };
+  keyframes?: string[];
+  audio?: string;
+}
+
+export interface StreamRecord extends RaRecord {
+  duration: number;
+  created_at: string;
+  description: string;
+  has_episodes: boolean;
+  prefix: string;
+  series_id: Identifier;
+  stream_date: string;
+  title: string;
+  updated_at: string;
+  stream_platform: string;
+  thumbnail_url: string;
+  video_clip_count: number;
+}
