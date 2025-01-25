01501af59c199de982ba76ac746f916f9b396771
saebyn <saebynx+github@gmail.com>
2025-01-25 07:30:45 -0800

    feat: implement exportEpisodeToCutList function and corresponding tests

---
+++ src/utilities/convertEpisodeToCutList.test.ts
@@ -0,0 +1,95 @@
+import type { Episode, Stream } from '@/types';
+import type { CutList } from '@saebyn/glowing-telegram-types';
+import { describe, expect, it } from 'vitest';
+import exportEpisodeToCutList from './convertEpisodeToCutList';
+
+describe('exportEpisodeToCutList', () => {
+  it('should create a cut list for a live stream episode with specific cuts', () => {
+    const episode: Episode = {
+      title: 'Episode Title',
+      description: 'Episode Description',
+      id: 'episode-id',
+      stream_id: 'stream-id',
+      tracks: [
+        { start: 'PT5M', end: 'PT20M' },
+        { start: 'PT20M', end: 'PT40M' },
+        { start: 'PT40M', end: 'PT59M30S' },
+      ],
+    };
+
+    const stream: Stream = {
+      series_id: 'series-id',
+      video_clips: [
+        { uri: '/bucket/clip1.mp4', duration: 60 * 20 },
+        { uri: '/bucket/clip2.mp4', duration: 60 * 20 },
+        { uri: '/bucket/clip3.mp4', duration: 60 * 20 },
+      ],
+    };
+
+    const frameRate = 60;
+    const cutList: CutList = exportEpisodeToCutList(episode, stream, frameRate);
+
+    expect(cutList).toMatchObject({
+      inputMedia: [
+        {
+          s3Location: '/bucket/clip1.mp4',
+          sections: [
+            { startFrame: 5 * 60 * frameRate, endFrame: 20 * 60 * frameRate },
+          ],
+        },
+        {
+          s3Location: '/bucket/clip2.mp4',
+          sections: [{ startFrame: 0, endFrame: 20 * 60 * frameRate }],
+        },
+        {
+          s3Location: '/bucket/clip3.mp4',
+          sections: [
+            { startFrame: 0, endFrame: 19 * 60 * frameRate + 30 * frameRate },
+          ],
+        },
+        {
+          s3Location: '/assets/outro.mp4',
+          sections: [{ startFrame: 0, endFrame: 1800 }],
+        },
+        {
+          s3Location: '/assets/intro.mp4',
+          sections: [{ startFrame: 0, endFrame: 3600 }],
+        },
+        {
+          s3Location: '/assets/twitch-watermark.mp4',
+          sections: [{ startFrame: 0, endFrame: 114 }],
+        },
+        {
+          s3Location: '/assets/like-sub.mp4',
+          sections: [{ startFrame: 0, endFrame: 300 }],
+        },
+      ],
+      outputTrack: [
+        { mediaIndex: 0, sectionIndex: 0 },
+        { mediaIndex: 1, sectionIndex: 0 },
+        { mediaIndex: 2, sectionIndex: 0 },
+        {
+          mediaIndex: 3,
+          sectionIndex: 0,
+          transitionIn: {
+            type: 'fadein',
+            duration: 5 * frameRate,
+          },
+        },
+      ],
+      overlayTracks: [
+        { mediaIndex: 4, sectionIndex: 0, startFrame: 0 },
+        { mediaIndex: 5, sectionIndex: 0, startFrame: frameRate * 30 },
+        { mediaIndex: 6, sectionIndex: 0, startFrame: frameRate * 60 },
+      ],
+      version: '1.0.0',
+      audioMixChanges: [
+        {
+          effect: 'fade',
+          endFrame: 196200,
+          startFrame: 195600,
+        },
+      ],
+    });
+  });
+});
---
+++ src/utilities/convertEpisodeToCutList.ts
@@ -0,0 +1,224 @@
+import type { Episode, Stream } from '@/types';
+import { parseIntoSeconds } from '@/utilities/isoDuration';
+import type { CutList } from '@saebyn/glowing-telegram-types';
+
+function buildCutListFromTracks(
+  episodeTracks: Episode['tracks'],
+  streamVideoClips: Stream['video_clips'],
+  frameRate: number,
+) {
+  return (
+    episodeTracks
+      // convert start and end times to frames and select video clips
+      // that are within the time range of each track
+      .flatMap(function getVideoClipsForTrack({ start, end }) {
+        const cutStartFrame = parseIntoSeconds(start) * frameRate;
+        const cutEndFrame = parseIntoSeconds(end) * frameRate;
+
+        let elapsedFrames = 0;
+
+        return (
+          streamVideoClips
+            // convert video clip durations to frames and calculate start and end
+            // frames for each clip based on the elapsed frames so far
+            .map(function mapVideoClipToFrames({ uri, duration }) {
+              const startFrame = elapsedFrames;
+
+              elapsedFrames += duration * frameRate;
+
+              return {
+                s3Location: uri,
+                startFrame,
+                endFrame: elapsedFrames,
+                relativeStartFrame: cutStartFrame - startFrame,
+                relativeEndFrame: cutEndFrame - startFrame,
+              };
+            })
+            // filter out video clips that are not within the time range of the track
+            .filter(function isClipWithinTimeRange({ startFrame, endFrame }) {
+              if (startFrame >= cutEndFrame) {
+                return false;
+              }
+
+              if (endFrame <= cutStartFrame) {
+                return false;
+              }
+
+              return true;
+            })
+        );
+      })
+      .reduce(
+        function groupByS3Location(
+          acc,
+          {
+            s3Location,
+
+            relativeStartFrame,
+            relativeEndFrame,
+          },
+        ) {
+          if (!acc[s3Location]) {
+            acc[s3Location] = [];
+          }
+
+          acc[s3Location].push({
+            startFrame: relativeStartFrame,
+            endFrame: relativeEndFrame,
+          });
+
+          return acc;
+        },
+        {} as Record<
+          string,
+          {
+            startFrame: number;
+            endFrame: number;
+          }[]
+        >,
+      )
+  );
+}
+
+export default function exportEpisodeToCutList(
+  episode: Episode,
+  stream: Stream,
+  frameRate = 60,
+): CutList {
+  const tracks = episode.tracks || [];
+  const videoClips = stream.video_clips || [];
+
+  // For each track, add which video clip to use and the start and end frames
+  // if the track spans multiple video clips, add multiple items to
+  // `simpleCutList`
+  // Skip any video clips that are prior to the start of the track
+  // Skip any video clips that are after the end of the track
+  // If a video clip is partially used, only use the portion that is needed
+  const simpleCutList = buildCutListFromTracks(tracks, videoClips, frameRate);
+
+  const [inputMedia, outputTrack] = Object.entries(simpleCutList).reduce(
+    ([inputMedia, outputTrack], [s3Location, sections]) => {
+      let mediaIndex = inputMedia.findIndex(
+        (media) => media.s3Location === s3Location,
+      );
+      if (mediaIndex === -1) {
+        mediaIndex = inputMedia.push({ s3Location, sections: [] }) - 1;
+      }
+
+      const media = inputMedia[mediaIndex];
+
+      for (const { startFrame, endFrame } of sections) {
+        media.sections.push({ startFrame, endFrame });
+        outputTrack.push({
+          mediaIndex,
+          sectionIndex: media.sections.length - 1,
+        });
+      }
+
+      return [inputMedia, outputTrack];
+    },
+    [[], []] as [CutList['inputMedia'], CutList['outputTrack']],
+  );
+
+  // index 1, outro
+  const outroIndex =
+    inputMedia.push({
+      s3Location: '/assets/outro.mp4',
+      sections: [{ startFrame: 0, endFrame: 1800 }],
+    }) - 1;
+  // Add outro to output track
+  outputTrack.push({
+    mediaIndex: outroIndex,
+    sectionIndex: 0,
+    transitionIn: {
+      type: 'fadein',
+      duration: 5 * frameRate, // 5 seconds
+    },
+  });
+
+  const totalOutputFrames = outputTrack.reduce(
+    (acc, { mediaIndex, sectionIndex, transitionIn, transitionOut }, index) => {
+      const media = inputMedia[mediaIndex];
+      const section = media.sections[sectionIndex];
+      let transitionFrames = 0;
+
+      // If this is not the first section, add the transition in duration
+      if (index > 0) {
+        transitionFrames += transitionIn?.duration || 0;
+      }
+      // If this is not the last section, add the transition out duration
+      if (index < outputTrack.length - 1) {
+        transitionFrames += transitionOut?.duration || 0;
+      }
+
+      // Subtract the transition frames from the total duration
+      // of the section to get the total output frames.
+      // This is the number of frames that will be output to the final video
+      // for this section.
+      // The transition frames are not included in the output because they
+      // are used to blend the sections together, so are already included
+      // in the previous or next section.
+      return acc + (section.endFrame - section.startFrame) - transitionFrames;
+    },
+    0,
+  );
+
+  // Create audio mix changes
+  const audioMixChanges: CutList['audioMixChanges'] = [
+    // fade in audio for outro
+    {
+      startFrame: totalOutputFrames - 35 * frameRate, // 35 seconds from the end
+      endFrame: totalOutputFrames - 25 * frameRate, // 25 seconds from the end
+      effect: 'fade',
+    },
+  ];
+
+  // Create overlay tracks
+  // hex iris effect
+  const hexIrisIndex =
+    inputMedia.push({
+      s3Location: '/assets/intro.mp4',
+      sections: [{ startFrame: 0, endFrame: 60 * frameRate }],
+    }) - 1;
+  // streamed live on twitch watermark
+  const twitchWatermarkIndex =
+    inputMedia.push({
+      s3Location: '/assets/twitch-watermark.mp4',
+      sections: [{ startFrame: 0, endFrame: 114 }],
+    }) - 1;
+  // like/sub reminder
+  const likeSubIndex =
+    inputMedia.push({
+      s3Location: '/assets/like-sub.mp4',
+      sections: [{ startFrame: 0, endFrame: 300 }],
+    }) - 1;
+
+  const overlayTracks: CutList['overlayTracks'] = [
+    // hex iris effect
+    {
+      mediaIndex: hexIrisIndex,
+      sectionIndex: 0,
+      startFrame: 0,
+    },
+    // streamed live on twitch watermark
+    {
+      mediaIndex: twitchWatermarkIndex,
+      sectionIndex: 0,
+      startFrame: frameRate * 30, // 30 seconds in
+    },
+    // like/sub reminder
+    {
+      mediaIndex: likeSubIndex,
+      sectionIndex: 0,
+      startFrame: frameRate * 60, // 60 seconds in
+    },
+  ];
+
+  return {
+    inputMedia,
+    outputTrack,
+    version: '1.0.0',
+    audioMixChanges,
+    overlayTracks,
+  };
+}
