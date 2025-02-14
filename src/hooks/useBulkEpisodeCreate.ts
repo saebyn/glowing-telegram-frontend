cb47fa4b9c30684ee1156a58df990373160961e2
saebyn <saebynx+github@gmail.com>
2025-02-13 17:58:24 -0800

    feat: useBulkEpisodeCreate populates the cut list

--- src/hooks/useBulkEpisodeCreate.ts
+++ src/hooks/useBulkEpisodeCreate.ts
@@ -1,8 +1,18 @@
+import convertEpisodeToCutList from '@/utilities/convertEpisodeToCutList';
 import { convertSecondsToISODuration } from '@/utilities/isoDuration';
-import type { Stream } from '@saebyn/glowing-telegram-types/src/types';
+import type {
+  Episode,
+  Stream,
+  VideoClip as StreamMedia,
+} from '@saebyn/glowing-telegram-types/src/types';
 import type { VideoClip } from '@saebyn/glowing-telegram-video-editor';
 import { useMutation } from '@tanstack/react-query';
-import { useDataProvider, useNotify, useReference } from 'react-admin';
+import {
+  useDataProvider,
+  useGetManyReference,
+  useNotify,
+  useReference,
+} from 'react-admin';
 
 export default function useBulkEpisodeCreate(stream: Stream | undefined) {
   const notify = useNotify();
@@ -18,6 +28,21 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
     options: { enabled: !!stream?.series_id },
   });
 
+  const {
+    data: streamMedia,
+    isLoading: isStreamMediaLoading,
+    error: streamMediaError,
+  } = useGetManyReference(
+    'video_clips',
+    {
+      target: 'stream_id',
+      id: stream?.id || '',
+    },
+    {
+      enabled: !!stream,
+    },
+  );
+
   const { mutate, isPending, error } = useMutation<void, Error, VideoClip[]>({
     mutationKey: ['bulkCreateEpisodes', stream?.id],
     mutationFn: async (clips) => {
@@ -25,25 +50,55 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
         return;
       }
 
+      if (!stream || !series || !streamMedia) {
+        return;
+      }
+
+      const processedMediaSegments = streamMedia.map(
+        (mediaSegment: StreamMedia) => ({
+          uri: mediaSegment.key,
+          duration: mediaSegment.metadata?.format?.duration || 0,
+          start_time: mediaSegment.start_time,
+        }),
+      );
+
+      processedMediaSegments.sort(
+        (a, b) => (a.start_time ?? 0) - (b.start_time ?? 0),
+      );
+
+      const streamWithVideoClips = {
+        ...stream,
+        video_clips: processedMediaSegments,
+        series_id: stream.series_id || null,
+      };
+
       const baseEpIndex = (series?.max_episode_order_index || 0) + 1;
+
+      const episodes: Partial<Episode>[] = clips
+        .map((clip, index) => ({
+          stream_id: stream.id,
+          series_id: stream.series_id,
+          order_index: baseEpIndex + index,
+          title: `${stream.title} - Episode ${baseEpIndex + index}`,
+          description: stream.description || '',
+          notify_subscribers: series.notify_subscribers,
+          category: series.category,
+          tags: series.tags,
+          tracks: [
+            {
+              start: convertSecondsToISODuration(clip.start),
+              end: convertSecondsToISODuration(clip.end),
+            },
+          ],
+        }))
+        .map((episode) => ({
+          ...episode,
+          cut_list: convertEpisodeToCutList(episode, streamWithVideoClips, 60),
+        }));
 
       await Promise.all([
         dataProvider.bulkCreate('episodes', {
-          data: clips.map((clip, index) => ({
-            stream_id: stream?.id,
-            series_id: stream?.series_id,
-            order_index: baseEpIndex + index,
-            title: `${stream?.title} - Episode ${baseEpIndex + index}`,
-            tracks: [
-              {
-                start: convertSecondsToISODuration(clip.start / 1000),
-                end: convertSecondsToISODuration(clip.end / 1000),
-              },
-            ],
-            notify_subscribers: series?.notify_subscribers,
-            category: series?.category,
-            tags: series?.tags,
-          })),
+          data: episodes,
         }),
         // update the stream with the has_episodes field
         dataProvider.update('streams', {
@@ -89,8 +144,8 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
 
   return {
     action: bulkCreateEpisodes,
-    isLoading: isLoadingSeries,
+    isLoading: isLoadingSeries || isStreamMediaLoading,
     isPending,
-    error: errorSeries || error,
+    error: errorSeries || streamMediaError || error,
   };
 }
