4f89f6f26fef9b269b53145cdea73603160ee911
saebyn <saebynx+github@gmail.com>
2025-02-15 11:56:04 -0800

    fix: :bug: Missed conversion from ms to seconds in useBulkEpisodeCreate hook

--- src/hooks/useBulkEpisodeCreate.ts
+++ src/hooks/useBulkEpisodeCreate.ts
@@ -43,59 +43,16 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
     },
   );
 
-  const { mutate, isPending, error } = useMutation<void, Error, VideoClip[]>({
+  const { mutate, isPending, error } = useMutation<
+    void,
+    Error,
+    Partial<Episode>[]
+  >({
     mutationKey: ['bulkCreateEpisodes', stream?.id],
-    mutationFn: async (clips) => {
+    mutationFn: async (episodes) => {
       if (isLoadingSeries) {
         return;
       }
-
-      if (!stream || !series || !streamMedia) {
-        return;
-      }
-
-      const processedMediaSegments = streamMedia.map(
-        (mediaSegment: StreamMedia) => ({
-          uri: mediaSegment.key,
-          duration: mediaSegment.metadata?.format?.duration || 0,
-          start_time: mediaSegment.start_time,
-        }),
-      );
-
-      processedMediaSegments.sort(
-        (a, b) => (a.start_time ?? 0) - (b.start_time ?? 0),
-      );
-
-      const streamWithVideoClips = {
-        ...stream,
-        video_clips: processedMediaSegments,
-        series_id: stream.series_id || null,
-      };
-
-      const baseEpIndex = (series?.max_episode_order_index || 0) + 1;
-
-      const episodes: Partial<Episode>[] = clips
-        .map((clip, index) => ({
-          stream_id: stream.id,
-          series_id: stream.series_id,
-          order_index: baseEpIndex + index,
-          title: `${stream.title} - Episode ${baseEpIndex + index}`,
-          description: stream.description || '',
-          notify_subscribers: series.notify_subscribers,
-          category: series.category,
-          tags: series.tags,
-          tracks: [
-            {
-              start: convertSecondsToISODuration(clip.start),
-              end: convertSecondsToISODuration(clip.end),
-            },
-          ],
-        }))
-        .map((episode) => ({
-          ...episode,
-          cut_list: convertEpisodeToCutList(episode, streamWithVideoClips, 60),
-        }));
-
       await Promise.all([
         dataProvider.bulkCreate('episodes', {
           data: episodes,
@@ -108,10 +65,13 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
         }),
       ]);
 
+      const newMaxEpisodeOrderIndex =
+        episodes[episodes.length - 1].order_index || 0;
+
       // update the series with the new max_episode_order_index
       await dataProvider.update('series', {
         id: series?.id,
-        data: { max_episode_order_index: baseEpIndex + clips.length - 1 },
+        data: { max_episode_order_index: newMaxEpisodeOrderIndex },
         previousData: series,
       });
     },
@@ -128,7 +88,53 @@ export default function useBulkEpisodeCreate(stream: Stream | undefined) {
       return;
     }
 
-    mutate(clips, {
+    if (!stream || !series || !streamMedia) {
+      return;
+    }
+
+    const processedMediaSegments = streamMedia.map(
+      (mediaSegment: StreamMedia) => ({
+        uri: mediaSegment.key,
+        duration: mediaSegment.metadata?.format?.duration || 0,
+        start_time: mediaSegment.start_time,
+      }),
+    );
+
+    processedMediaSegments.sort(
+      (a, b) => (a.start_time ?? 0) - (b.start_time ?? 0),
+    );
+
+    const streamWithVideoClips = {
+      ...stream,
+      video_clips: processedMediaSegments,
+      series_id: stream.series_id || null,
+    };
+
+    const baseEpIndex = (series?.max_episode_order_index || 0) + 1;
+
+    const episodes: Partial<Episode>[] = clips
+      .map((clip, index) => ({
+        stream_id: stream.id,
+        series_id: stream.series_id,
+        order_index: baseEpIndex + index,
+        title: `${stream.title} - Episode ${baseEpIndex + index}`,
+        description: stream.description || '',
+        notify_subscribers: series.notify_subscribers,
+        category: series.category,
+        tags: series.tags,
+        tracks: [
+          {
+            start: convertSecondsToISODuration(clip.start / 1000),
+            end: convertSecondsToISODuration(clip.end / 1000),
+          },
+        ],
+      }))
+      .map((episode) => ({
+        ...episode,
+        cut_list: convertEpisodeToCutList(episode, streamWithVideoClips, 60),
+      }));
+
+    mutate(episodes, {
       onSuccess: () => {
         // tell the user that the episodes were created
         notify('gtk.bulk_create_episodes.success', {
