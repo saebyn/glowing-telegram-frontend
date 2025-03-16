281ef3f83b0d189a8d4705f132b0d12893f82a60
saebyn <saebynx+github@gmail.com>
2025-03-16 11:10:56 -0700

    feat: update theme imports to use radiant themes instead of nano themes

--- src/ra/theme.ts
+++ src/ra/theme.ts
@@ -1,5 +1,8 @@
 import { deepmerge } from '@mui/utils';
-import { nanoDarkTheme, nanoLightTheme } from 'react-admin';
+import {
+  radiantDarkTheme as baseDarkTheme,
+  radiantLightTheme as baseLightTheme,
+} from 'react-admin';
 
 const themeOverrides = {
   // Disable injecting global styles for MuiInputBase
@@ -12,5 +15,5 @@ const themeOverrides = {
   },
 };
 
-export const lightTheme = deepmerge(nanoLightTheme, themeOverrides);
-export const darkTheme = deepmerge(nanoDarkTheme, themeOverrides);
+export const lightTheme = deepmerge(baseLightTheme, themeOverrides);
+export const darkTheme = deepmerge(baseDarkTheme, themeOverrides);
