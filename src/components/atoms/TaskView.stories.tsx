b8e60113b58b5e26725340f1358e3de0a228d2df
saebyn <saebynx+github@gmail.com>
2025-03-25 17:49:01 -0700

    feat: migrate TaskView component to atoms and update imports in TaskListView

---
+++ src/components/atoms/TaskView.stories.tsx
@@ -0,0 +1,25 @@
+import type { Task } from '@saebyn/glowing-telegram-types';
+import TaskView from './TaskView';
+
+import { action } from '@storybook/addon-actions';
+export default {
+  title: 'Atoms/TaskView',
+  component: TaskView,
+  tags: ['atoms'],
+
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      record_id: '12131-123123-123123-123123',
+      status: 'RUNNING',
+      created_at: '2021-09-01T12:00:00Z',
+      updated_at: '2021-09-01T12:00:00Z',
+      task_type: "rendering",
+    } as Task,
+    lastViewedTaskTimestamp: new Date('2021-09-01T12:00:00Z'),
+    markViewed: action('markViewed'),
+  },
+};
