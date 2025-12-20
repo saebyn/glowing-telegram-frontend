5ee107511b90286c4f814ded661ad48262121d17
saebyn <me@saebyn.dev>
2025-12-20 08:26:09 -0800

    Refactor WebsocketProvider to use static URL and remove token handling

--- src/ra/Layout.tsx
+++ src/ra/Layout.tsx
@@ -1,18 +1,13 @@
 import type { FC } from 'react';
-import { Layout, useGetIdentity } from 'react-admin';
+import { Layout } from 'react-admin';
 import { WebsocketProvider } from '@/hooks/useWebsocket';
 import AppBar from './AppBar';
 
 const { VITE_WEBSOCKET_URL: WEBSOCKET_URL } = import.meta.env;
 
 const MyLayout: FC<{ children?: React.ReactNode }> = ({ children }) => {
-  const identity = useGetIdentity();
-  const accessToken = identity?.data?.idToken;
-  const websocketUrl = new URL(WEBSOCKET_URL);
-  websocketUrl.searchParams.set('token', accessToken || '');
-
   return (
-    <WebsocketProvider url={accessToken ? websocketUrl.toString() : null}>
+    <WebsocketProvider url={WEBSOCKET_URL}>
       <Layout appBar={AppBar}>{children}</Layout>
     </WebsocketProvider>
   );
